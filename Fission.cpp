c80848e6af3d7bd13699661d0964069a061f4ce5
Căluianu Andrei-Răzvan <140332707+VICHiNG16@users.noreply.github.com>
2025-01-13 12:34:42 +0200

    Delete Fission.cpp

--- Fission.cpp
+++
@@ -1,9 +0,0 @@
-#include "Fission.h"
-#include <iostream>
-
-double Fission::simulateFission(const Nucleus& nucleus) {
-    double releasedEnergy = nucleus.getBindingEnergy() * 0.75;
-    std::cout << "Fission of nucleus " << nucleus.getName()
-        << " released " << releasedEnergy << " MeV.\n";
-    return releasedEnergy;
-}
