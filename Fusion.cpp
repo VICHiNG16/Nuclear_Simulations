405971a4dd7c77f826edc323a9aa535079313371
Căluianu Andrei-Răzvan <140332707+VICHiNG16@users.noreply.github.com>
2025-01-13 12:34:53 +0200

    Delete Fusion.cpp

--- Fusion.cpp
+++
@@ -1,9 +0,0 @@
-#include "Fusion.h"
-#include <iostream>
-
-double Fusion::simulateFusion(const Nucleus& nucleus1, const Nucleus& nucleus2) {
-    double releasedEnergy = (nucleus1.getBindingEnergy() + nucleus2.getBindingEnergy()) * 0.85;
-    std::cout << "Fusion of nuclei " << nucleus1.getName() << " and " << nucleus2.getName()
-        << " released " << releasedEnergy << " MeV.\n";
-    return releasedEnergy;
-}
